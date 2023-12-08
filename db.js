f9f630b827a864f83a7c805db17be1d68b361101
sehyeon An <sehyeon014@gmail.com>
2023-12-08 15:35:08 +0900

    fix: db.js

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
 const mysql = require("mysql");
-require('dotenv').config();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
+  host: 'sc-database-service',
+  port: 3306,
+  user: 'root',
+  password: 'qwerty',
+  database: 'sc_db',
   dateStrings: "date",
 });
 
