70039bc80c7c0e22edf0b202b3df33316319049e
You Jeong Jang <sunny819819@naver.com>
2023-12-10 14:35:41 +0900

    Update app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const expressSession = require("express-session");
 var server = require('http').createServer(app);
 var io = require('socket.io')(server, {
   cors: true,
-  origins: ['http://sc-frontend-service:80'],
+  origins: ['http://sc-chatting.com:80'],
 });
 
 const mainRouter = require("./routes/main");
